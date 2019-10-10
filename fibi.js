eee519991e52b535a30efd400cc586bc922c30f6
Pavel <slabkopg@gmail.com>
2019-10-10 17:50:04 +0300

    [IMP] method

--- fibi.js
+++ fibi.js
@@ -1,17 +1,48 @@
-var num = [0, 1];
+/*
+* recomendations
+* https://habr.com/ru/post/449616/
+*/
 function fib(n) {
    
     if (n < 0) {
         return false;
-    } else if (n == 0) {
+    } else if (n === 0) {
         return 0;
-    } else if (n == 1) {
+    } else if (n === 1) {
         return 1;
-    } else {
- 
-        for (var i = 2; i <= n; i++) {
-        num[i] = num[i - 1] + num[i - 2];      
-        }
     }
-return num[n];
-}    
+
+    const sequence = [0, 1];  // bad idea
+ 
+    for (let i = 2; i <= n; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+
+    return sequence[n];
+}
+
+
+// better solution
+// you don't need to store "sequence" - only prev and next
+function fib(n) {
+    
+    if (n < 0) {
+        return false;
+    }
+    
+    let prev = 0, next = 1;
+
+    for(let i = 0; i < n; i++){
+        let temp = next;
+        next = prev + next;
+        prev = temp;
+    }
+    return prev;
+}
+
+
+
+// test
+fib(3)  // 2
+fib(7)  // 13
+fib(17)  // 1597
